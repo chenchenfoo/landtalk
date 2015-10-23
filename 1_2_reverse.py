e13fc144d0d0eec786b8e063010ecde7bfd07297
chenchenfoo <chenchenfoo@gmail.com>
2015-10-23 14:29:23 -0500

    Create 1_2_reverse.py

---
+++ 1_2_reverse.py
@@ -0,0 +1,11 @@
+def reverse(s):
+    return s[::-1]
+
+def main():
+    stringVal=input("Please enter the string")
+    a = reverse(stringVal)
+    print(a)
+    
+
+if __name__ == '__main__':
+    main()
